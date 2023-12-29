582519d05e7b89a4cf07e7186acf00ccb7ae6dd7
yaruneki <l.casillo02072002@gmail.com>
2023-12-29 23:53:37 +0100

    added loading to ListaPazienti

--- src/app/components/gestione_app/ListaPazienti.tsx
+++ src/app/components/gestione_app/ListaPazienti.tsx
@@ -1,16 +1,19 @@
-import { Paziente } from 'app/interfaces/gestione_autenticazione/Paziente';
-import PazienteService from 'app/services/gestione_autenticazione/PazienteService';
 import React, { useEffect, useState } from 'react';
 import ElementoLista from './ElementoLista';
+import { Paziente } from 'app/interfaces/gestione_autenticazione/Paziente';
+import PazienteService from 'app/services/gestione_autenticazione/PazienteService';
 
 function ListaPazienti(): JSX.Element {
   const [pazienti, setPazienti] = useState<Paziente[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const fetchData = async (): Promise<void> => {
     const pazienteService = new PazienteService();
 
     try {
       const data = await pazienteService.fetchPazienti();
       setPazienti(data);
+      setIsLoading(false);
     } catch (error) {
       console.error('Error fetching pazienti:', error);
     }
@@ -23,9 +26,15 @@ function ListaPazienti(): JSX.Element {
   return (
     <div className="ListaPazienti">
       <header className="ListaPazieti-header"></header>
-      {pazienti.map((paziente, index) => {
-        return ElementoLista(index, paziente.nome, paziente.cognome);
-      })}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : pazienti.length === 0 ? (
+        <p>Non ci sono pazienti.</p>
+      ) : (
+        pazienti.map((paziente, index) => {
+          return ElementoLista(index, paziente.nome, paziente.cognome);
+        })
+      )}
     </div>
   );
 }

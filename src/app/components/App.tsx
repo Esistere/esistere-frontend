a128d4b76bf4c5cff7c7540178b36a6dd4e42527
Marygio <giovannadellap@gmail.com>
2024-01-04 23:24:40 +0100

    add component CreaToDoList

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './Navbar';
+import CreaToDoList from './gestione_app/CreaToDoList';
 
 function App(): JSX.Element {
   return (
---
+++ src/app/components/gestione_app/CreaToDoList.tsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import { ToDoList } from 'app/interfaces/gestione_todolist/ToDoList';
+//import { Attivita } from 'app/interfaces/gestione_todolist/Attivita';
+import 'app/css/gestione_app/FormElements.css';
+import { TextField, Button } from '@mui/material';
+
+function CreaToDoList(): JSX.Element {
+  const [NumAttivita, setNumAttivita] = useState<number | null>(null);
+  const [attivitaConfermate, setAttivitaConfermate] = useState<number[]>([]);
+
+  /*const handleChangeNum = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const { value } = event.target;
+    setNumAttivita(parseInt(value));
+    console.log(JSON.stringify(NumAttivita));
+  };*/
+  const handleChangeNum = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const { value } = event.target;
+    setNumAttivita(parseInt(value));
+  };
+
+  useEffect(() => {
+    if (NumAttivita !== null) {
+      console.log(JSON.stringify(NumAttivita));
+    }
+  }, [NumAttivita]);
+
+  const handleConfermaClick = (): void => {
+    if (NumAttivita !== null) {
+      const nuovaListaAttivita = Array.from(
+        { length: NumAttivita },
+        (_, index) => index + 1
+      );
+      setAttivitaConfermate(nuovaListaAttivita);
+    }
+  };
+
+  return (
+    <>
+      <form style={{ display: 'flex', flexWrap: 'wrap', width: '50%' }}>
+        <div className="riga">
+          <h2>Quante attività vuoi inserire in questa ToDoList? </h2>
+          <TextField
+            required
+            type="number"
+            id="outline-num_attivita"
+            label="Numero attività"
+            style={{
+              width: '16.15em',
+              margin: '1em',
+              boxSizing: 'border-box',
+            }}
+            onChange={handleChangeNum}
+          />
+        </div>
+        <div>
+          <Button
+            type="button"
+            variant="contained"
+            onClick={handleConfermaClick}
+          >
+            Conferma
+          </Button>
+        </div>
+        <br />
+      </form>
+      <form className="formflex">
+        {attivitaConfermate.map((attivita, index) => (
+          <div key={index} className="riga">
+            <TextField
+              key={index}
+              name={`attivita_${attivita}`}
+              label={`Attività ${attivita}`}
+              id="outlined-basic"
+              style={{
+                width: '16.15em',
+                margin: '1em',
+                boxSizing: 'border-box',
+              }}
+            />
+            <br />
+          </div>
+        ))}
+      </form>
+    </>
+  );
+}
+export default CreaToDoList;

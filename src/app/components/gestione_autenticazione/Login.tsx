c980eff4302f1ecdfeb9e4cbe77a27a503ee665c
yaruneki <l.casillo02072002@gmail.com>
2024-01-14 00:15:21 +0100

    fix login

--- src/app/components/gestione_autenticazione/Login.tsx
+++ src/app/components/gestione_autenticazione/Login.tsx
@@ -18,6 +18,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserType } from './UserProvider';
 import Pulsante from '../gestione_app/Pulsante';
+import Navbar from '../Navbar';
 
 const theme = createTheme({
   palette: {
@@ -87,108 +88,111 @@ function Login(): JSX.Element {
   };
 
   return (
-    <form method="post" onSubmit={handleSubmit}>
-      <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Card
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              padding: (theme) => theme.spacing(3),
-              backgroundColor: '#EDE7F6',
-              borderRadius: '10px',
-              boxShadow: '0 3px 5px 2px rgba(155, 105, 135, .3)',
-              color: '#5E35B1',
-            }}
-          >
-            <img
-              src={logo}
-              alt="Logo"
-              style={{ height: '100px', marginTop: '20px' }}
-            />
-            <Typography
-              component="h1"
-              variant="h5"
-              color="primary"
-              sx={{ mt: 2, fontWeight: 'bold' }}
+    <>
+      <Navbar />
+      <form method="post" onSubmit={handleSubmit}>
+        <ThemeProvider theme={theme}>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Card
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                padding: (theme) => theme.spacing(3),
+                backgroundColor: '#EDE7F6',
+                borderRadius: '10px',
+                boxShadow: '0 3px 5px 2px rgba(155, 105, 135, .3)',
+                color: '#5E35B1',
+              }}
             >
-              Accedi
-            </Typography>
-            <CardContent>
-              <Box sx={{ mt: 1 }}>
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="email"
-                  label="Indirizzo Email"
-                  name="email"
-                  autoComplete="email"
-                  autoFocus
-                  variant="outlined"
-                  color="primary"
-                  onChange={handleChange}
-                />
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="passwd"
-                  label="Password"
-                  id="password"
-                  autoComplete="current-password"
-                  variant="outlined"
-                  color="primary"
-                  onChange={(event) => handleChange(event)}
-                  type={showPassword ? 'text' : 'password'}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          onClick={handleClickShowPassword}
-                          onMouseDown={handleMouseDownPassword}
-                          edge="end"
-                        >
-                          {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-                <Pulsante
-                  tipologia="scuro"
-                  testo="Accedi"
-                  nome="login"
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                  color="primary"
-                />
-
-                <Typography variant="body2" sx={{ mb: 2 }}>
-                  Non hai un account?{' '}
-                  <Link
-                    to="/registrazione"
-                    style={{
-                      fontWeight: 'bold',
-                      textDecoration: 'none',
-                      color: 'inherit',
+              <img
+                src={logo}
+                alt="Logo"
+                style={{ height: '100px', marginTop: '20px' }}
+              />
+              <Typography
+                component="h1"
+                variant="h5"
+                color="primary"
+                sx={{ mt: 2, fontWeight: 'bold' }}
+              >
+                Accedi
+              </Typography>
+              <CardContent>
+                <Box sx={{ mt: 1 }}>
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Indirizzo Email"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    variant="outlined"
+                    color="primary"
+                    onChange={handleChange}
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="passwd"
+                    label="Password"
+                    id="password"
+                    autoComplete="current-password"
+                    variant="outlined"
+                    color="primary"
+                    onChange={(event) => handleChange(event)}
+                    type={showPassword ? 'text' : 'password'}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                          >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
                     }}
-                  >
-                    Registrati qui
-                  </Link>
-                </Typography>
-              </Box>
-            </CardContent>
-          </Card>
-        </Container>
-      </ThemeProvider>
-    </form>
+                  />
+                  <Pulsante
+                    tipologia="scuro"
+                    testo="Accedi"
+                    nome="login"
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    color="primary"
+                  />
+
+                  <Typography variant="body2" sx={{ mb: 2 }}>
+                    Non hai un account?{' '}
+                    <Link
+                      to="/registrazione"
+                      style={{
+                        fontWeight: 'bold',
+                        textDecoration: 'none',
+                        color: 'inherit',
+                      }}
+                    >
+                      Registrati qui
+                    </Link>
+                  </Typography>
+                </Box>
+              </CardContent>
+            </Card>
+          </Container>
+        </ThemeProvider>
+      </form>
+    </>
   );
 }
 

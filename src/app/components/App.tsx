43e5823037e4dfbd2d8202734253c0da054129a1
yaruneki <l.casillo02072002@gmail.com>
2024-01-13 20:44:12 +0100

    update conditional home

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './Navbar';
+import { useNavigate } from 'react-router-dom';
+import { UserType } from './gestione_app/UserProvider';
 
 function App(): JSX.Element {
+  const navigate = useNavigate();
+  const userType: string | null = localStorage.getItem('id');
+
+  useEffect(() => {
+    if (userType !== null) {
+      const userTypeNumber = Number(userType);
+      if (userTypeNumber === UserType.caregiver) {
+        navigate('/caregiver');
+        window.location.reload();
+      } else if (userTypeNumber === UserType.medico) {
+        navigate('/medico');
+        window.location.reload();
+      }
+    }
+  }, [navigate, userType]);
+
   return (
     <>
       <Navbar />
@@ -9,4 +27,5 @@ function App(): JSX.Element {
     </>
   );
 }
+
 export default App;

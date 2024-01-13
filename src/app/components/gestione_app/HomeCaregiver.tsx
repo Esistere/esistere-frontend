bd6242d6fab9c76eb2550cd4f24d29cba00266b2
yaruneki <l.casillo02072002@gmail.com>
2024-01-13 20:43:57 +0100

    update Homes

--- src/app/components/gestione_app/HomeCaregiver.tsx
+++ src/app/components/gestione_app/HomeCaregiver.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../Navbar';
 
 const HomeCaregiver: React.FC = () => {
   return (
     <div>
+      <Navbar />
       <h2>Home Caregiver</h2>
     </div>
   );
--- src/app/components/gestione_app/HomeMedico.tsx
+++ src/app/components/gestione_app/HomeMedico.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../Navbar';
 
 const HomeMedico: React.FC = () => {
   return (
     <div>
+      <Navbar />
       <h2>Home Medico</h2>
     </div>
   );

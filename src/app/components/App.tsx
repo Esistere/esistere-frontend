a0763056fc37fce314f8b94c264d88023518dce3
rosacarota <92106994+rosacarota@users.noreply.github.com>
2024-01-15 12:58:28 +0100

    fix home

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -2,8 +2,6 @@ import React, { useEffect } from 'react';
 import { useUser, UserType } from './gestione_autenticazione/UserProvider';
 import { useNavigate } from 'react-router-dom';
 import Caricamento from './gestione_app/Caricamento';
-import HomeMedico from './gestione_autenticazione/medico/HomeMedico';
-import HomeCaregiver from './gestione_autenticazione/caregiver/HomeCaregiver';
 
 function App(): JSX.Element {
   const { userType, loading } = useUser();
@@ -23,17 +21,11 @@ function App(): JSX.Element {
     return <Caricamento />;
   }
 
-  if (userType === UserType.medico) {
-    return <HomeMedico />;
-  } else if (userType === UserType.caregiver) {
-    return <HomeCaregiver />;
-  } else {
-    return (
-      <>
-        <h2>Home</h2>
-      </>
-    );
-  }
+  return (
+    <>
+      <h2>Home</h2>
+    </>
+  );
 }
 
 export default App;

35b24b481dfcd238bae10d0dca719275f1e977e3
Antonio <35410603+redyz13@users.noreply.github.com>
2024-01-16 21:37:32 +0100

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,6 @@ import ListaQuizAllenamento from './app/components/gestione_quiz_allenamento/Lis
 import CreaStoria from './app/components/gestione_storia/CreaStoria';
 import CreaToDoList from './app/components/gestione_todolist/CreaToDoList';
 import Navbar from './app/components/Navbar';
-import Footer from './app/components/Footer';
 import reportWebVitals from './app/reportWebVitals';
 import withCaregiver from './app/components/gestione_autenticazione/caregiver/CaregiverHOC';
 import withMedico from './app/components/gestione_autenticazione/medico/MedicoHOC';
@@ -95,7 +94,6 @@ root.render(
             element={React.createElement(withCaregiver(CreaToDoList))}
           />
         </Routes>
-        <Footer />
       </HashRouter>
     </UserProvider>
   </React.StrictMode>

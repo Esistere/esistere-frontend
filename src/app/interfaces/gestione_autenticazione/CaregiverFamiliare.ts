1ce88e19e476ce49858f3b85ff93e0eea19ae1ad
yaruneki <l.casillo02072002@gmail.com>
2024-01-17 02:50:35 +0100

    code refactor

--- src/app/interfaces/gestione_autenticazione/CaregiverFamiliare.ts
+++ src/app/interfaces/gestione_autenticazione/CaregiverFamiliare.ts
@@ -1,12 +1,12 @@
 export interface CaregiverFamiliare {
-  codice_identificativo?: number | undefined,
-  nome: string,
-  cognome: string,
-  indirizzo: string,
-  citta: string,
-  numero_civico: string,
-  data_di_nascita: Date,
-  numero_telefono: string,
-  email: string,
-  passwd: string
+  codice_identificativo?: number | undefined;
+  nome: string;
+  cognome: string;
+  indirizzo: string;
+  citta: string;
+  numero_civico: string;
+  data_di_nascita: Date;
+  numero_di_telefono: string;
+  email: string;
+  passwd: string;
 }

87b720c27f13fe7a99cda9af29adca548970d2e3
yaruneki <l.casillo02072002@gmail.com>
2024-01-20 17:18:50 +0100

    update RispostaQuizAllenamento

--- src/app/interfaces/gestione_quiz_allenamento/RispostaQuizAllenamento.ts
+++ src/app/interfaces/gestione_quiz_allenamento/RispostaQuizAllenamento.ts
@@ -1,6 +1,6 @@
 export interface RispostaQuizAllenamento {
   id?: number | undefined;
-  domanda_ag: number;
+  domanda_ag?: number | undefined;
   risposta: string;
   corretta?: boolean | undefined;
   selezionata?: boolean | undefined;

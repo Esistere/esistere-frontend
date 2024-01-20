62d6ea9e0d0039705d091570d50d2db824d678a5
vale-99dr <valentinodragone@hotmail.it>
2024-01-20 19:35:30 +0100

    update CreaToDoList

--- src/app/components/gestione_todolist/CreaToDoList.tsx
+++ src/app/components/gestione_todolist/CreaToDoList.tsx
@@ -2,9 +2,18 @@ import React, { useState, useEffect } from 'react';
 //eslint-disable-next-line
 import { ToDoList } from 'app/interfaces/gestione_todolist/ToDoList';
 import 'app/css/gestione_app/FormElements.css';
-import { TextField } from '@mui/material';
+import { TextField, Typography, Grid, Paper } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Pulsante from '../gestione_app/Pulsante';
+import Navbar from '../Navbar';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#8A2BE2',
+    },
+  },
+});
 function CreaToDoList(): JSX.Element {
   const [NumAttivita, setNumAttivita] = useState<number | null>(null);
   const [attivitaConfermate, setAttivitaConfermate] = useState<number[]>([]);
@@ -13,7 +22,14 @@ function CreaToDoList(): JSX.Element {
     event: React.ChangeEvent<HTMLInputElement>
   ): void => {
     const { value } = event.target;
-    setNumAttivita(parseInt(value));
+    console.log(value);
+    let newNumAttivita = parseInt(value);
+
+    if (isNaN(newNumAttivita) || newNumAttivita < 0) {
+      newNumAttivita = 0;
+    }
+
+    setNumAttivita(newNumAttivita);
   };
 
   useEffect(() => {
@@ -23,20 +39,26 @@ function CreaToDoList(): JSX.Element {
   }, [NumAttivita]);
 
   const handleConfermaClick = (): void => {
-    if (NumAttivita !== null) {
+    if (NumAttivita !== null && NumAttivita >= 0) {
       const nuovaListaAttivita = Array.from(
         { length: NumAttivita },
         (_, index) => index + 1
       );
       setAttivitaConfermate(nuovaListaAttivita);
+    } else {
+      setAttivitaConfermate([]);
     }
   };
   const [testo1] = useState<string>('conferma');
   return (
-    <>
-      <form style={{ display: 'flex', flexWrap: 'wrap', width: '50%' }}>
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <form className="formflex">
+        <Typography variant="h4" style={{ color: 'blueviolet' }}>
+          Quante attività vuoi inserire in questa ToDoList?
+        </Typography>
+
         <div className="riga">
-          <h2>Quante attività vuoi inserire in questa ToDoList? </h2>
           <TextField
             required
             type="number"
@@ -47,10 +69,13 @@ function CreaToDoList(): JSX.Element {
               margin: '1em',
               boxSizing: 'border-box',
             }}
+            inputProps={{
+              min: 0,
+            }}
             onChange={handleChangeNum}
           />
         </div>
-        <div>
+        <div className="riga">
           {Pulsante({
             tipologia: 'scuro',
             testo: testo1,
@@ -60,27 +85,49 @@ function CreaToDoList(): JSX.Element {
             onClick: handleConfermaClick,
           })}
         </div>
-        <br />
-      </form>
-      <form className="formflex">
-        {attivitaConfermate.map((attivita, index) => (
-          <div key={index} className="riga">
-            <TextField
-              key={index}
-              name={`attivita_${attivita}`}
-              label={`Attività ${attivita}`}
-              id="outlined-basic"
-              style={{
-                width: '16.15em',
-                margin: '1em',
-                boxSizing: 'border-box',
-              }}
-            />
-            <br />
+
+        {attivitaConfermate.length > 0 && (
+          <div>
+            <Grid container spacing={3} style={{ marginTop: '2em' }}>
+              <Grid item xs={12}>
+                <Paper
+                  elevation={3}
+                  style={{
+                    padding: '16px',
+                    width: '100%',
+                    margin: 'auto',
+                    textAlign: 'center',
+                  }}
+                >
+                  <form className="formflex">
+                    <Typography variant="h6" style={{ color: 'blueviolet' }}>
+                      Descrivi le attività:
+                    </Typography>
+                    {attivitaConfermate.map((attivita, index) => (
+                      <div key={index} className="riga">
+                        <TextField
+                          key={index}
+                          name={`attivita_${attivita}`}
+                          label={`Attività ${attivita}`}
+                          id="outlined-basic"
+                          style={{
+                            width: '30.5em',
+                            margin: '1em',
+                            boxSizing: 'border-box',
+                          }}
+                        />
+                        <br />
+                      </div>
+                    ))}
+                  </form>
+                </Paper>
+              </Grid>
+            </Grid>
           </div>
-        ))}
+        )}
       </form>
-    </>
+    </ThemeProvider>
   );
 }
+
 export default CreaToDoList;

ba50e4748b725e89b583b3dc7365e08ac77018b1
vale-99dr <valentinodragone@hotmail.it>
2024-01-21 16:09:33 +0100

    update Caricamento

--- src/app/components/gestione_app/Caricamento.tsx
+++ src/app/components/gestione_app/Caricamento.tsx
@@ -1,21 +1,13 @@
 import * as React from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import 'app/css/gestione_app/Caricamento.css';
-import { Stack } from '@mui/material';
-
 function Caricamento(): JSX.Element {
   return (
-    <Stack
-      direction={{ xs: 'column', md: 'row' }}
-      spacing={2}
-      alignItems="center"
-      justifyContent="center"
-      sx={{ width: '100%' }}
-    >
+    <div className="allContainer">
       <CircularProgress
         sx={{ color: 'blueviolet', width: '10em', height: '10em' }}
       />
-    </Stack>
+    </div>
   );
 }
 

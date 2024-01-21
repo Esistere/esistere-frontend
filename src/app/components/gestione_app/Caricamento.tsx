196ee7d2a27db670ac0a59c21fcb4f69abdd6033
yaruneki <l.casillo02072002@gmail.com>
2024-01-21 15:11:41 +0100

    update Caricamento

--- src/app/components/gestione_app/Caricamento.tsx
+++ src/app/components/gestione_app/Caricamento.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import 'app/css/gestione_app/Caricamento.css';
+import { Stack } from '@mui/material';
 
 function Caricamento(): JSX.Element {
   return (
-    <div className="CircProgContainer">
-      <CircularProgress style={{ color: 'blueviolet' }} />
-    </div>
+    <Stack
+      direction={{ xs: 'column', md: 'row' }}
+      spacing={2}
+      alignItems="center"
+      justifyContent="center"
+      sx={{ width: '100%' }}
+    >
+      <CircularProgress
+        sx={{ color: 'blueviolet', width: '10em', height: '10em' }}
+      />
+    </Stack>
   );
 }
 
